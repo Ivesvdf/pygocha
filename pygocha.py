a66484ca2024fef76ecd90d9f3657ecb642d8117
Ives van der Flaas <ives.vdf@gmail.com>
2010-07-08 01:41:49 +0200

    Moved demo stuff to separate file

--- pygocha.py
+++ pygocha.py
@@ -81,22 +81,6 @@ class OverlappedBarChart(PyGoCha):
 	def graphName(self):
 		return "bvo"
 
-pgc = ThreeDPie()
-pgc.title('My Favorite Bars')
-pgc.datapoint(28, "McGee's")
-pgc.datapoint(30, "MacLauren's")
-pgc.datapoint(12, "White Horse Tavern")
-pgc.datapoint(3, "King Cole Bar")
-pgc.datapoint(27, "P&amp;G")
-print pgc.getURL(600,300)
 
-print ''
-
-two = TwoDPie()
-two.title("Pacman")
-two.datapoint(80, "Percentage of pie that is Pacman")
-two.datapoint(20, "Percentage of pie that is not Pacman")
-two.rotate(0.628)
-print two.getURL(700,300)
 
 
