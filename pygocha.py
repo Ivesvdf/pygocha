63670499e65d848029836cf8ea97f347f792b9a1
Ives van der Flaas <ives.vdf@gmail.com>
2010-07-12 01:08:39 +0200

    Custom axis labels are allowed

--- pygocha.py
+++ pygocha.py
@@ -5,6 +5,11 @@ class PyGoCha:
 		self.__legendOnly = False
 		self.__data = list() 
 		self.__labels = list() 
+		self.__margins = (0,0,0,0)
+		self.__visibleAxes = list()
+		self.__xStep = None
+		self.__yStep = None
+		self.__customAxes = dict()
 
 	def title(self, newTitle):
 		self.__title = newTitle
@@ -18,6 +23,9 @@ class PyGoCha:
 	def label(self, label):
 		self.__labels.append(label)
 
+	def margins(self, left, right, top, bottom):
+		self.__margins = (left, right, top, bottom)
+
 	def getURL(self, width, height):
 		# Place the contents of the working series in 
 		# data
@@ -29,41 +37,100 @@ class PyGoCha:
 		if self.__title != '':
 			url += '&chtt=' + self.__title
 
+		if self.__margins != (0,0,0,0):
+			marginStrs =  a = map(lambda x:str(x), self.__margins)
+			url += '&chma=' + ','.join(marginStrs)
+
 		url += "&chs=" + str(width) + "x" + str(height)
 		url += "&chd=t:"
 
-		total = 0
 
 		pointStrings = list()
 
-		for series in self.__data:
-			for point in series:
-				total += point
+		maxima = map(max, self.__data)
+		minima = map(min, self.__data)
+		maxVal = max(maxima)
+		minVal = min(minima)
 
+		# Rescale values
+		for series in self.__data:
 			rescaled = list()
 
 			for point in series:
-				rescaled.append(str(100.0*point/total))
+				rescaled.append(str(100.0*point/maxVal))
 			
 			pointStrings.append(','.join(rescaled))
 
 		url += '|'.join(pointStrings)
+		
+		# Correct & show axis labels
+		if len(self.__visibleAxes) > 0:
+			if minVal < 0:
+				zero = minVal
+			else:
+				zero = 0
+			url += '&chxt=' + ','.join(self.__visibleAxes)
+
+		# If the user enters a step value, we change range for that
+		# axis
+		if self.__yStep != None:
+			url += '&chxr=' + str(self.__visibleAxes.index('y')) \
+					+ ',' + str(zero) + ',' + str(maxVal)  \
+					+ ',' + str(self.__yStep)
+
+
+		# Pass trough axis values if the user enters custom labels
+		# for each point on some axis
+		if len(self.__customAxes) > 0:
+			url += '&chxl='
+
+			s = ''
+
+			for axis, labels in self.__customAxes.items():
+				s += str(self.__visibleAxes.index(axis)) \
+						+ ':|' \
+						+ '|'.join(labels) \
+						+ '|'
+
+			url += s[0:-1]
+
 
 		if self.__legendOnly:
 			url += '&chdl='
-		else:
+		elif len(self.__labels) > 0:
 			url += '&chl='
 
 		url += '|'.join(self.__labels)
+
 		url += self._afterURL()
 
 		return url
 		
 	def newSeries(self):
 		self.__data.append(self.__thisSeries)
+		self.__thisSeries = list()
 
 	def _afterURL(self):
 		return ''
+
+#	def showXAxis(self, step):
+#		self.__visibleAxes.append('x')
+#		self.__xStep = step
+
+	def showYAxis(self, step):
+		self.__visibleAxes.append('y')
+		self.__yStep = step
+
+
+	def addCustomAxisLabel(self, axis, label):
+		if axis not in self.__visibleAxes:
+			self.__visibleAxes.append(axis)
+
+		if not self.__customAxes.has_key(axis):
+			self.__customAxes[axis] = list()
+
+		self.__customAxes[axis].append(label)
+
 
 class Pie(PyGoCha):
 	__rotate = 0.0
@@ -91,15 +158,44 @@ class TwoDPie(Pie):
 	def graphName(self):
 		return "p"
 
-class GroupedBarChart(PyGoCha):
+class BarChart(PyGoCha):
+	def __init__(self):
+		PyGoCha.__init__(self)
+		self.__seriesColors = list()
+		self.__barColors = list()
+
+	def setSeriesColor(self, color):
+		self.__seriesColors.append(color)
+
+	def setBarColor(self, color):
+		self.__barColors.append(color)
+
+	def bar(self, data, label):
+		self.data(data)
+		self.label(label)
+
+	def _afterURL(self):
+		extra = ''
+
+		if len(self.__seriesColors) > 0:
+			extra += '&chco=' + ','.join(self.__seriesColors)
+
+		if len(self.__barColors) > 0:
+			extra += '&chco=' + '|'.join(self.__barColors)
+
+		return extra
+
+
+
+class GroupedBarChart(BarChart):
 	def graphName(self):
 		return "bvg"
 
-class StackedBarChart(PyGoCha):
+class StackedBarChart(BarChart):
 	def graphName(self):
 		return "bvs"
 
-class OverlappedBarChart(PyGoCha):
+class OverlappedBarChart(BarChart):
 	def graphName(self):
 		return "bvo"
 
